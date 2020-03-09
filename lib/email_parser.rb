44704908fce6bc53f841c2d1c6bda920981c438c
Alexia Adediran <alexiaadediran30@gmail.com>
2020-03-09 01:02:05 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,36 +1,15 @@
-# class EmailParser
-
-#   attr_accessor :email
-#   def  initialize(emails)
-#     @email = emails
-#   end
-
-#   def parse
-#     email_array = @email.split(/[, ]/).uniq
-#     email_array.reject! {|element| element.empty?}
-#     email_array
-#   end
-
-# end
 class EmailParser
-attr_accessor :emails 
 
-def initialize(emails)
-  @emails = emails
-end 
+  attr_accessor :email
+  def  initialize(emails)
+    @email = emails
+  end
 
-def parse
-  puts splitEmail = emails.split.collect {|address| address.split(',')} 
-  returnArr = splitEmail.flatten.uniq
-  returnArr
+  def parse
+    email_array = @email.split(/[, ]/).uniq
+    email_array.reject! {|element| element.empty?}
+    email_array
+  end
+
 end
 
-end
-
-emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
-parser1 = EmailParser.new(emails1)
-parser1.parse 
-
-emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
-parser2 = EmailParser.new(emails2)
-parser2.parse
